2cb20d36ac442866be3c9a5fac06f9358e3d3336
Mann <chanmannlim@gmail.com>
2011-05-02 10:58:35 +0700

    install software dependencies

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+# install gvim
+sudo apt-get update
+sudo apt-get -y install curl git-core vim vim-gnome
+sudo apt-get upgrade
+
+# clone environment
+git clone git://github.com/lchanmann/mgvim.git
+
