bc815fb7aed804e968474a3bea2a74d85eb68dcb
Mann <chanmannlim@gmail.com>
2011-05-02 11:33:48 +0700

    install gvim and restore vim default configuration

--- install.sh
+++ install.sh
@@ -2,9 +2,27 @@
 
 # install gvim
 sudo apt-get update
-sudo apt-get -y install curl git-core vim vim-gnome
+sudo apt-get -y install git-core vim vim-gnome
 sudo apt-get upgrade
 
 # clone environment
-git clone git://github.com/lchanmann/mgvim.git
+if ! which gvim >/dev/null; then
+  git clone git://github.com/lchanmann/mgvim.git
 
+  vimrc=$HOME/.vimrc
+  gvimrc=$HOME/.gvimrc
+  vim_dir=$HOME/.vim
+
+  # backup 
+  mv $vimrc $vimrc.bak
+  mv $gvimrc $gvimrc.bak
+  mv $vim_dir $vim_dir.bak
+
+  # configure .vimrc and .gvimrc
+  mv mgvim/vimrc $vimrc
+  mv mgvim/gvimrc $gvimrc
+
+  # finalize installation
+  rm -rf mgvim/.git mgvim/.gitignore mgvim/install.sh
+  mv -f mgvim $vim_dir
+fi
